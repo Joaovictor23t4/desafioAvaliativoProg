e29e4b3ad49bd995be985b6b01005f808808eb1d
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-11-28 16:58:51 -0300

    continuando atividade

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let ulPendente = document.querySelector("#ulPendentes");
+let inputPendente = document.querySelector("#inputPendente");
 let botaoAdicionar = document.querySelector("#btnAdicionar");
+let listBtnFinish;
 let arrayPendentes = [];
 
 function adicionarTarefa() {
     let input = document.querySelector("#inputPendente");
     let li = document.createElement("li");
-    li.innerHTML = `${input.value} - <a>Finalizar</a>`;
+    li.classList.add("tarefa");
+    li.innerHTML = `${input.value} - <button class="btnFinalizar" onclick="finalizarTarefa()">Finalizar</button>`;
     ulPendente.appendChild(li);
     arrayPendentes.push(input.value);
+    listBtnFinish = document.getElementsByClassName("btnFinalizar");
+    console.log(listBtnFinish);
 }
 
 function limparInput() {
@@ -15,7 +20,19 @@ function limparInput() {
     input.value = "";
 }
 
+function finalizarTarefa() {
+    let liRemove = document.getElementsByClassName("tarefa");
+    ulPendente.removeChild(liRemove[this]);
+}
+
 botaoAdicionar.addEventListener("click", () => {
     adicionarTarefa();
     limparInput();
+});
+
+inputPendente.addEventListener("keypress", (event) => {
+    if (event.keyCode === 13) {
+        adicionarTarefa();
+        limparInput(); 
+    }
 });
